93772dc5201918de74a2c4f7ce6bb7aeb082da0b
Joseph Leiferman <joseph.leiferman@gmail.com>
2016-11-07 20:39:33 -0600

    got rid of mockFloor
    Who ever added this, added broken code

--- Group_B3_Project/src/warehouse_system/floor/FloorTest.java
+++ Group_B3_Project/src/warehouse_system/floor/FloorTest.java
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-package Floor;
+
+package warehouse_system.floor;
 import java.util.ArrayList;
 
 /**
@@ -18,25 +18,5 @@ public class FloorTest {
         
     }
 }
-=======
-package warehouse_system.floor;
 
-import java.util.ArrayList;
 
-/**
- *
- * @author josephtleiferman
- */
-public class FloorTest {
-    public static void main(String[] args) {
-    	Floor test = new MockFloor();
-        ArrayList<MockFloor.Directions> routes= new ArrayList<>();
-        int[] dock = {5,0};
-        int[] charger1 = {2,0};
-        System.out.println("getting Route");
-        routes = test.getRoute(dock,charger1);
-        System.out.println(routes.toString());
-        
-    }
-}
->>>>>>> origin/master
