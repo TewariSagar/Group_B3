0dfd5c663d15fad6843b9ef1c1b9bf722e0b1a9b
NickEBarnes <nicholas-barnes@uiowa.edu>
2016-11-07 20:27:53 -0600

    Create Bin.java
    Hashmap for an order, needs some integeration with the orders

---
+++ Group_B3_Project/src/warehouse_system/belt/Bin.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Bin extends Belt {
+    //for binList, 0 is starting position, 19 is last position
+    
+	//Hashmap for an <orderNumber, itemID>
+	Map<Integer, Integer> order = new HashMap<Integer, Integer>();
+    
+	
+//	public int binBin = 1;
+    
+    public Bin(){
+    	 order.put(Belt.orderNum, 322);
+    }
+    
+    //this is where orders will be saved to a bin, will need to wait
+    //for integration otherwise I am not too sure how to do that
+}
