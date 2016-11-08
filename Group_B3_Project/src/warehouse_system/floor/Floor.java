deac7c601f092efaa3623e3e3f9c430909207b62
Joseph Leiferman <joseph.leiferman@gmail.com>
2016-11-07 20:34:47 -0600

    getting ride of code I didn't not add

--- Group_B3_Project/src/warehouse_system/floor/Floor.java
+++ Group_B3_Project/src/warehouse_system/floor/Floor.java
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-package Floor;
+
+package warehouse_system.floor;
 
 import java.util.HashMap;
 import java.util.ArrayList;
@@ -146,21 +146,4 @@ public class Floor {
         
     }
 }
-=======
-package warehouse_system.floor;
 
-import java.util.ArrayList;
-
-import warehouse_system.floor.MockFloor.Directions;
-import warehouse_system.robot.Robot;
-
-public interface Floor{
-	
-	public static final int width = 60, height = 30;				// unit: grid
-	public static final int gridSize = 10;							// [10 pixel x 10 pixel] each grid
-	
-	public ArrayList<Directions> getRoute(int[] start, int[] end);
-	public void placeRobot(Robot r);
-	
-}
->>>>>>> origin/master
