cf4b78f8d8e064cd26488de12fccb357e6d511c6
Joseph Leiferman <joseph.leiferman@gmail.com>
2016-11-10 14:30:23 -0600

    Added shelve class
    will be able to store and remove items on a shelve
    contains a bare items class so that you can put items of type item that
    contain weight and name

---
+++ Group_B3_Project/src/warehouse_system/floor/Shelve.java
@@ -0,0 +1,34 @@
+package warehouse_system.floor;
+import java.util.HashMap;
+
+public class Shelve {
+	private HashMap<String,Item> itemsOnShelves = new HashMap<>();
+	
+	public void put(String n, Item i) {
+		itemsOnShelves.put(n, i);
+	}
+	public void remove(String n) {
+		itemsOnShelves.remove(n);
+	}
+}
+class Item {
+	private double weight;
+	private String name;
+	
+	public Item(double weight,String name) {
+		this.weight = weight;
+		this.name = name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	public String getName() {
+		return name;
+	}
+	public double getWeight() {
+		return weight;
+	}
+}
